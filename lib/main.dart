f25aaa7527ff0049af5c6c37664af3ae6ac519f0
RuqayaLaith <ruqayalaith98@gmail.com>
2022-09-03 03:19:09 +0300

    testt

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:talabateyproject/talabatey/page1/pagetalabatey.dart';
+import 'package:talabateyproject/totersproject/page1/page1.dart';
 
 void main() {
   runApp((MaterialApp(
